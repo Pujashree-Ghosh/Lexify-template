cd8485d953356097a5df5d99aa4fc296aeec6968
Ismail-Sarkar <ismail.sarkar@bitcanny.com>
2021-12-15 11:43:31 +0530

    listing type page done

--- src/components/CreateListing/CreateListing.js
+++ src/components/CreateListing/CreateListing.js
@@ -4,54 +4,105 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { pathByRouteName } from '../../util/routes';
 import routeConfiguration from '../../routeConfiguration';
-import { intlShape, injectIntl } from '../../util/reactIntl';
+import { FormattedMessage, injectIntl, intlShape } from '../../util/reactIntl';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
 import { setListingCategory } from '../../ducks/user.duck';
+import {
+  Page,
+  UserNav,
+  LayoutSingleColumn,
+  LayoutWrapperTopbar,
+  LayoutWrapperMain,
+  LayoutWrapperFooter,
+  Footer,
+  NamedLink,
+  FieldRadioButton,
+} from '../../components';
+import { TopbarContainer } from '../../containers';
+
+import css from './CreateListing.module.css';
 
 function CreateListingPageComponent(props) {
-  const { onSetListingCategory, history } = props;
+  const { onSetListingCategory, history, scrollingDisabled, intl } = props;
   const routes = routeConfiguration();
+  const title = intl.formatMessage({ id: 'ProfileSettingsPage.title' });
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          // onSetListingCategory('publicOral');
-          history.push(pathByRouteName('NewListingPublicOralPage', routes, {}));
-        }}
-      >
-        Public Oral
-      </button>
-      <button
-        onClick={() => {
-          // onSetListingCategory('customOral');
-          history.push(pathByRouteName('NewListingCustomOralPage', routes, {}));
-        }}
-      >
-        Custom Oral
-      </button>
-      <button
-        onClick={() => {
-          // onSetListingCategory('customService');
-          history.push(pathByRouteName('NewListingCustomServicePage', routes, {}));
-        }}
-      >
-        Custom Service
-      </button>
-      {/* <NamedLin */}
-    </div>
+    <Page className={css.root} title={title} scrollingDisabled={scrollingDisabled}>
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer currentPage="CreateListing" />
+          <UserNav selectedPageName="CreateListing" />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperMain>
+          <div className={css.content}>
+            <div className={css.headingContainer}>
+              <h3 className={css.heading}>
+                <FormattedMessage id="CreateListing.heading" />
+              </h3>
+            </div>
+            <div className={css.subHeadingContainer}>
+              <h4 className={css.subHeading}>
+                <FormattedMessage id="CreateListing.subHeading" />
+              </h4>
+            </div>
+            <div className={css.typeContainer}>
+              <div className={css.oral}>
+                <button
+                  onClick={() => {
+                    history.push(pathByRouteName('NewListingPublicOralPage', routes, {}));
+                  }}
+                >
+                  Public Oral
+                </button>
+                <div className={css.infoText}>
+                  <FormattedMessage id="CreateListing.oralInfo" />
+                </div>
+              </div>
+              <div className={css.custom}>
+                <button
+                  onClick={() => {
+                    history.push(pathByRouteName('NewListingCustomOralPage', routes, {}));
+                  }}
+                >
+                  Custom Oral
+                </button>
+                <div className={css.infoText}>
+                  <FormattedMessage id="CreateListing.customInfo" />
+                </div>
+              </div>
+              <div className={css.service}>
+                <button
+                  onClick={() => {
+                    history.push(pathByRouteName('NewListingCustomServicePage', routes, {}));
+                  }}
+                >
+                  Custom Service
+                </button>
+                <div className={css.infoText}>
+                  <FormattedMessage id="CreateListing.serviceInfo" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </LayoutWrapperMain>
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSingleColumn>
+    </Page>
   );
 }
-// const mapStateToProps = state => {
-//   const { category } = state.user;
-//   console.log(category);
-//   return { category };
-// };
+const mapStateToProps = state => {
+  const { category } = state.user;
+  return { scrollingDisabled: isScrollingDisabled(state) };
+};
 // const mapDispatchToProps = dispatch => ({
 //   onSetListingCategory: category => dispatch(setListingCategory(category)),
 // });
 const CreateListing = compose(
-  withRouter
-  // connect(mapStateToProps, mapDispatchToProps)
+  withRouter,
+  connect(mapStateToProps)
 )(injectIntl(CreateListingPageComponent));
 
 export default CreateListing;
--- src/components/UserNav/UserNav.js
+++ src/components/UserNav/UserNav.js
@@ -11,34 +11,41 @@ import { getListingType, createSlug } from '../../util/urlHelpers';
 import css from './UserNav.module.css';
 
 const listingTab = (listing, selectedPageName) => {
-  if (!listing) {
-    return {
-      text: <FormattedMessage id="UserNav.newListing" />,
-      selected: selectedPageName === 'NewListingPage',
-      linkProps: {
-        name: 'NewListingPage',
-      },
-    };
-  }
-  const currentListing = ensureOwnListing(listing);
-  const id = currentListing.id.uuid;
-  const { title = '', state } = currentListing.attributes;
-  const slug = createSlug(title);
-  const isDraft = state === LISTING_STATE_DRAFT;
-
   return {
-    text: <FormattedMessage id="UserNav.editListing" />,
-    selected: selectedPageName === 'EditListingPage',
+    text: <FormattedMessage id="UserNav.newListing" />,
+    selected: selectedPageName === 'CreateListing',
     linkProps: {
-      name: 'EditListingPage',
-      params: {
-        id,
-        slug,
-        type: getListingType(isDraft),
-        tab: 'photos',
-      },
+      name: 'CreateListing',
     },
   };
+  // if (!listing) {
+  //   return {
+  //     text: <FormattedMessage id="UserNav.newListing" />,
+  //     selected: selectedPageName === 'NewListingPage',
+  //     linkProps: {
+  //       name: 'NewListingPage',
+  //     },
+  //   };
+  // }
+  // const currentListing = ensureOwnListing(listing);
+  // const id = currentListing.id.uuid;
+  // const { title = '', state } = currentListing.attributes;
+  // const slug = createSlug(title);
+  // const isDraft = state === LISTING_STATE_DRAFT;
+
+  // return {
+  //   text: <FormattedMessage id="UserNav.editListing" />,
+  //   selected: selectedPageName === 'EditListingPage',
+  //   linkProps: {
+  //     name: 'EditListingPage',
+  //     params: {
+  //       id,
+  //       slug,
+  //       type: getListingType(isDraft),
+  //       tab: 'photos',
+  //     },
+  //   },
+  // };
 };
 
 const UserNav = props => {
